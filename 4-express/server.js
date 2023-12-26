9675df02e7c2242f51ae4854f0d5925081062215
Shreyas <sonureigns007@gmail.com>
2023-12-26 19:07:10 +0530

    .env file added for mongo

--- 4-express/server.js
+++ 4-express/server.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
-const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD); // data base connection string from .env
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
