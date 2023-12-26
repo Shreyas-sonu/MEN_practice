3975b22d7c374519d886a2ee7a318ad240acab2e
Shreyas <sonureigns007@gmail.com>
2023-12-26 19:08:21 +0530

    tour helper linked with route

--- 4-express/routes/tourRoutes.js
+++ 4-express/routes/tourRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const tourHelper = require('../controllers/tourHandlers');
 
 const tourRouter = express.Router();
 
-tourRouter.route('/').get().post();
+tourRouter.route('/').get(tourHelper.getAllTours).post(tourHelper.createTour);
 
-tourRouter.route('/:id').get().patch().delete();
+tourRouter
+  .route('/:id')
+  .get(tourHelper.getTour)
+  .patch(tourHelper.editTour)
+  .delete(tourHelper.deleteTour);
 
 module.exports = tourRouter;
