a3957d3b8d871ade2e543103b4e4a2adfc471a03
Shreyas <sonureigns007@gmail.com>
2023-12-27 16:22:17 +0530

    tour routes added

--- 4-express/routes/tourRoutes.js
+++ 4-express/routes/tourRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const {
+  createTour,
+  getAllTours,
+  getTour,
+  deleteTour,
+  patchTour,
+} = require('./../controllers/tourHandlers');
 
 const tourRouter = express.Router();
+tourRouter.route('/').get(getAllTours).post(createTour);
 
-tourRouter.route('/').get().post();
-
-tourRouter.route('/:id').get().patch().delete();
+tourRouter.route('/:id').get(getTour).patch(patchTour).delete(deleteTour);
 
 module.exports = tourRouter;
