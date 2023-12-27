824138c74c0a4f9cbd5a8f2e175f63d263c6126a
Shreyas <sonureigns007@gmail.com>
2023-12-27 16:22:31 +0530

    route handler added

--- 4-express/controllers/tourHandlers.js
+++ 4-express/controllers/tourHandlers.js
@@ -1,5 +1,49 @@
 const fs = require('fs');
+const path = require('path');
 
-const tours = fs.readFileSync(`${__dirname}/../dev-data/data/tours`);
+console.log(path.join(__dirname, '../dev-data/data/tours-simple.json'));
+// const tours = JSON.parse(
+//   fs.readFileSync(path.join(__dirname,'../dev-data/data/tours-simple.json'))
+// );
 
-console.log(tours);
+// exports.getAllTours = (req, res) => {
+//   res.status(200).json({
+//     status: 'Success',
+//     data: tours,
+//     length: tours.length,
+//   });
+// };
+// exports.getTour = (req, res) => {
+//   res.status(200).json({
+//     status: 'Success',
+//     data: tours.filter((e) => e.id === Number(req.param))[0],
+//   });
+// };
+// exports.createTour = (req, res) => {
+//   const newData = { id: tours.length, ...req.body };
+//   const latestData = tours.push(newData);
+//   fs.writeFile('../dev-data/data/tours-simple.json', latestData);
+//   res.status(201).json({
+//     status: 'Created',
+//     data: newData,
+//     length: tours.length,
+//   });
+// };
+// exports.patchTour = (req, res) => {
+//   const id = Number(req.param);
+//   const dataToPatch = tours.filter((e) => e.id === Number(req.params))[0];
+//   const newData = { dataToPatch, ...req.body };
+//   const latestData = tours;
+//   tours[id] = newData;
+//   fs.writeFile('../dev-data/data/tours-simple.json', latestData);
+//   res.status(200).json({
+//     status: 'Updated',
+//     data: tours.filter((e) => e.id === Number(req.params))[0],
+//   });
+// };
+// exports.deleteTour = (req, res) => {
+//   res.status(204).json({
+//     status: 'Success',
+//     data: null,
+//   });
+// };
