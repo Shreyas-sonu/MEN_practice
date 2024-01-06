d1bc68735c25515524e9da19301d52b0c20668f8
Shreyas <sonureigns007@gmail.com>
2024-01-06 19:20:31 +0530

    code cleanup for data base integration

--- 4-express/routes/tourRoutes.js
+++ 4-express/routes/tourRoutes.js
@@ -3,15 +3,12 @@ const {
   createTour,
   getAllTours,
   getTour,
-  checkId,
   deleteTour,
   patchTour,
-  checkBody,
 } = require('./../controllers/tourHandlers');
 
 const tourRouter = express.Router();
-tourRouter.param('id', checkId)
-tourRouter.route('/').get(getAllTours).post(checkBody,createTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
 tourRouter.route('/:id').get(getTour).patch(patchTour).delete(deleteTour);
 
 module.exports = tourRouter;
