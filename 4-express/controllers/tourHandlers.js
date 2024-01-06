b47de45769542facfdfd9bfa5e4b672d12966f32
Shreyas <sonureigns007@gmail.com>
2024-01-06 19:58:13 +0530

    updated query added in controller

--- 4-express/controllers/tourHandlers.js
+++ 4-express/controllers/tourHandlers.js
@@ -43,10 +43,19 @@ exports.getTour = async (req, res) => {
     res.status(404).json({ status: 'no data', error: error });
   }
 };
-exports.editTour = async (req, res) => {
+exports.deleteTour = (req, res) => {
+  console.log(toursForDuration);
+  res.status(204).json({
+    data: null,
+  });
+};
+exports.patchTour = async (req, res) => {
   const id = req.params.id;
   try {
-    const data = await Tour.replaceOne({ _id: id }, req.body);
+    const data = await Tour.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
     res.status(200).json({
       status: 'Updated',
       data: data,
@@ -54,30 +63,6 @@ exports.editTour = async (req, res) => {
   } catch (error) {
     res.status(400).json({ status: 'Failed', message: error });
   }
-};
-exports.deleteTour = (req, res) => {
-  console.log(toursForDuration);
-  res.status(204).json({
-    data: null,
-  });
-};
-exports.patchTour = (req, res) => {
-  const id = req.params.id;
-  const dataToPatch = tours.filter((e) => e.id === Number(req.params.id))[0];
-  const newData = { ...dataToPatch, ...req.body };
-  const latestData = tours;
-  tours[id] = newData;
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours.json`,
-    JSON.stringify(latestData),
-    (err) => {
-      console.log(err, 'error while patching');
-    }
-  );
-  res.status(200).json({
-    status: 'Updated',
-    data: tours.filter((e) => e.id === Number(req.params))[0],
-  });
 };
 exports.deleteTour = (req, res) => {
   res.status(204).json({
