529c2c2cab5f95f3b52f02981a04f1a6c0135f14
Shreyas <sonureigns007@gmail.com>
2024-01-07 20:50:22 +0530

    extra fields added in tour model

--- 4-express/model/tourModel.js
+++ 4-express/model/tourModel.js
@@ -5,18 +5,56 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: [
-      true,`Name Should be Unique`,
-    ],
+    unique: true,
+    trim: true,
   },
-  rating: {
+  slug: String,
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price bitch'],
+    required: [true, 'A tour must have a price'],
   },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 const Tour = mongoose.model('Tour', tourSchema);
 
