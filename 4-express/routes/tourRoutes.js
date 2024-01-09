8a42605e22a3998a6a17da736520499fcdf733b0
Shreyas <sonureigns007@gmail.com>
2024-01-09 23:09:35 +0530

    ai aliasing trial

--- 4-express/routes/tourRoutes.js
+++ 4-express/routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
   createTour,
+  top5Tour,
   getAllTours,
   getTour,
   deleteTour,
@@ -8,6 +9,7 @@ const {
 } = require('./../controllers/tourHandlers');
 
 const tourRouter = express.Router();
+tourRouter.route('/top-5').get(top5Tour, getAllTours);
 tourRouter.route('/').get(getAllTours).post(createTour);
 tourRouter.route('/:id').get(getTour).patch(patchTour).delete(deleteTour);
 
