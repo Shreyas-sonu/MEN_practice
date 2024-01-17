d0aa4a7c665fd82c04b755d5ef2e5f50786a8d77
Shreyas <sonureigns007@gmail.com>
2024-01-17 16:05:34 +0530

    user route helpers blocked temporarily

--- 4-express/routes/userRoutes.js
+++ 4-express/routes/userRoutes.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const {
-  createUser,
-  top5User,
-  monthlyPlanUsers,
-  getAllUsers,
-  getUserStats,
-  getUser,
-  deleteUser,
-  patchUser,
-} = require('./../controllers/userHandlers');
+// const express = require('express');
+// const {
+//   createUser,
+//   top5User,
+//   monthlyPlanUsers,
+//   getAllUsers,
+//   getUserStats,
+//   getUser,
+//   deleteUser,
+//   patchUser,
+// } = require('./../controllers/userHandlers');
 
-const userRouter = express.Router();
-userRouter.route('/monthly-plan/:year').get(monthlyPlanUsers);
-userRouter.route('/top-5').get(top5User, getAllUsers);
-userRouter.route('/stats').get(getUserStats);
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUser).patch(patchUser).delete(deleteUser);
+// const userRouter = express.Router();
+// userRouter.route('/monthly-plan/:year').get(monthlyPlanUsers);
+// userRouter.route('/top-5').get(top5User, getAllUsers);
+// userRouter.route('/stats').get(getUserStats);
+// userRouter.route('/').get(getAllUsers).post(createUser);
+// userRouter.route('/:id').get(getUser).patch(patchUser).delete(deleteUser);
 
-module.exports = userRouter;
+// module.exports = userRouter;
