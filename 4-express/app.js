c28e23e4e49c6a25d7473063b49cabd4ee075e1d
Shreyas <sonureigns007@gmail.com>
2024-03-28 16:31:01 +0530

    use routed blocked

--- 4-express/app.js
+++ 4-express/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
+// const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
